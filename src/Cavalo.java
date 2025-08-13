ceb71899584fb60124c2768e5d0da491ec1531a2
joodanic <a94015@alunos.uminho.pt>
2025-08-13 18:41:33 +0100

    Peao tinha erro no gerarMovimentos onde nao gerava corretamente os movimentos de captura diagonal e faltava algumas verificacoes

--- src/Cavalo.java
+++ src/Cavalo.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Cavalo extends Peca{
@@ -7,6 +8,27 @@ public class Cavalo extends Peca{
 
     @Override
     public List<Move> gerarMovimentos(Board tabuleiro) {
-        return null;
+        List<Move> movimentos = new ArrayList<>();
+        int[][] posicoesCavalo = {
+                {2, 1}, {2, -1}, {-2, 1}, {-2, -1},
+                {1, 2}, {1, -2}, {-1, 2}, {-1, -2}
+        };
+
+        Posicao atual = getPosicao();
+
+        for(int[] posicaoCavalo : posicoesCavalo){
+            int linha = atual.getLinha() + posicaoCavalo[0];
+            int coluna = atual.getColuna() + posicaoCavalo[1];
+
+            Peca queroIr = tabuleiro.obterPeca(linha,coluna);
+
+            if(queroIr!=null && queroIr.getCor()!=this.getCor()){
+                Posicao dest = new Posicao(linha,coluna);
+                Peca capturada = (queroIr instanceof Vazia) ? null : queroIr;
+                Move nova = new Move(atual,dest,this,capturada);
+                movimentos.add(nova);
+            }
+        }
+        return movimentos;
     }
 }
--- src/Peao.java
+++ src/Peao.java
@@ -13,46 +13,55 @@ public class Peao extends Peca{
 
         Posicao atual = getPosicao();
         Cor cor = getCor();
+        int linhaOrig = atual.getLinha();
+        int colunaOrig = atual.getColuna();
 
         int direcao = (cor == Cor.BRANCA) ? -1:1;
 
         if((cor == Cor.BRANCA && atual.getLinha()==6) || cor==Cor.PRETA && atual.getLinha()==1){
-            int linhaOrig = atual.getLinha();
             int linhaDest = linhaOrig+direcao;
             int linhaDest2 = linhaOrig + (2*direcao);
             Posicao dest = new Posicao(linhaDest,atual.getColuna());
             Posicao dest2 = new Posicao(linhaDest2,atual.getColuna());
-            Move moveCasa = new Move(atual,dest, this,tabuleiro.obterPeca(linhaDest,atual.getColuna()));
-            Move move2Casa = new Move(atual,dest2,this,tabuleiro.obterPeca(linhaDest2,atual.getColuna()));
-            movimentos.add(moveCasa);
-            movimentos.add(move2Casa);
+            Peca queroIr = tabuleiro.obterPeca(linhaDest,atual.getColuna());
+            Peca queroIr2 = tabuleiro.obterPeca(linhaDest2,atual.getColuna());
+
+            if(queroIr instanceof Vazia){
+                Move moveCasa = new Move(atual,dest, this,tabuleiro.obterPeca(linhaDest,atual.getColuna()));
+                movimentos.add(moveCasa);
+            }
+
+            if(queroIr instanceof Vazia && queroIr2 instanceof Vazia){
+                Move move2Casa = new Move(atual,dest2,this,tabuleiro.obterPeca(linhaDest2,atual.getColuna()));
+                movimentos.add(move2Casa);
+            }
         }
         else {
             //um movimento em frente, pois ja nao estamos na posicao original
-            int linhaOrig = atual.getLinha();
-            int colunaOrig = atual.getColuna();
             Peca frente = tabuleiro.obterPeca(linhaOrig+direcao,colunaOrig);
-            Peca direita = tabuleiro.obterPeca(linhaOrig+direcao,colunaOrig+1);
-            Peca esquerda = tabuleiro.obterPeca(linhaOrig+direcao,colunaOrig-1);
 
             if (frente instanceof Vazia){
                 Posicao dest = new Posicao(linhaOrig+direcao,colunaOrig);
                 Move moveCasa = new Move(atual,dest,this,frente);
                 movimentos.add(moveCasa);
             }
-
-            if(direita!=null){
-                Posicao dest = new Posicao(linhaOrig+direcao,colunaOrig+1);
-                Move moveCasa = new Move(atual,dest,this,direita);
-                movimentos.add(moveCasa);
-            }
-
-            if(esquerda!=null){
-                Posicao dest = new Posicao(linhaOrig+1,colunaOrig-1);
-                Move moveCasa = new Move(atual,dest,this,esquerda);
-                movimentos.add(moveCasa);
-            }
         }
+
+        Peca direita = tabuleiro.obterPeca(linhaOrig+direcao,colunaOrig+1);
+        Peca esquerda = tabuleiro.obterPeca(linhaOrig+direcao,colunaOrig-1);
+
+        if(!(direita instanceof Vazia) && direita.getCor()!=this.getCor()){
+            Posicao dest = new Posicao(linhaOrig+direcao,colunaOrig+1);
+            Move moveCasa = new Move(atual,dest,this,direita);
+            movimentos.add(moveCasa);
+        }
+
+        if(!(esquerda instanceof Vazia) && esquerda.getCor()!=this.getCor()){
+            Posicao dest = new Posicao(linhaOrig+direcao,colunaOrig-1);
+            Move moveCasa = new Move(atual,dest,this,esquerda);
+            movimentos.add(moveCasa);
+        }
+
         return movimentos;
     }
 }
